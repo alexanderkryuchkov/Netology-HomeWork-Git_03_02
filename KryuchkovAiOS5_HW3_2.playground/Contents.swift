6c4fa62450ef19cb917d4fe2a2bc861dbe97d0c8
Alexander Kryuchkov <alkryuchkov@mail.ru>
2022-02-20 18:28:48 +1100

    Ex. 1 - Enum added

---
+++ KryuchkovAiOS5_HW3_2.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+// ЗАДАЧА №1
+
+// Создание Enum
+enum DaysOfWeek {
+    static let sunday = 0
+    static let monday = 1
+    static let tuesday = 2
+    static let wednesday = 3
+    static let thursday = 4
+    static let friday = 5
+    static let saturday = 6
+}
