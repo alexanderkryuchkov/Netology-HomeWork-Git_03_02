1ca30efb67b37a1ace617457b6ac29fa457a7ca8
Alexander Kryuchkov <alkryuchkov@mail.ru>
2022-02-20 18:38:30 +1100

    Ex. 1 - Struct added

--- KryuchkovAiOS5_HW3_2.playground/Contents.swift
+++ KryuchkovAiOS5_HW3_2.playground/Contents.swift
@@ -12,3 +12,23 @@ enum DaysOfWeek {
     static let friday = 5
     static let saturday = 6
 }
+
+// Создание структуры
+struct JobApplication {
+    var firstName: String
+    var secondName: String
+    var age: Int
+    var education: String
+}
+
+// Создание экземпляров структур
+let personOne = JobApplication(firstName: "Иван", secondName: "Иванов", age: 32, education: "Высшее")
+
+// Создание экземпляров структур
+let personTwo = JobApplication(firstName: "Сергей", secondName: "Сергеев", age: 24, education: "Среднее")
+
+// Проверка
+print("Анкета соискателя: \(personOne.firstName)  \(personOne.secondName), возраст: \(personOne.age), образование: \(personOne.education)")
+
+print("Анкета соискателя: \(personTwo.firstName)  \(personTwo.secondName), возраст: \(personTwo.age), образование: \(personTwo.education)")
+
